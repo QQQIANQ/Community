fdf483410a454720d6e13994a05564be6468f927
QIAN MA <maqian@hotmail.fr>
2020-03-23 14:30:27 +0100

    add check cookies

--- src/main/java/com/example/community/controller/AutorizeController.java
+++ src/main/java/com/example/community/controller/AutorizeController.java
@@ -7,13 +7,12 @@ import com.example.community.mapper.UserMapper;
 import com.example.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -36,7 +35,7 @@ public class AutorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request){
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -48,15 +47,16 @@ public class AutorizeController {
         if(githubuser!=null){
 
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubuser.getName());
             user.setAccountId(String.valueOf(githubuser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            request.getSession().setAttribute("githubuser",githubuser);
+            response.addCookie(new Cookie("token", token));
             return "redirect:/";//redirection
-            // login success cookie and session
+            // login success
         }
         else {
             return "redirect:/";
