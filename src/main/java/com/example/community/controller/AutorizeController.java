3ec2f24ba5258b7d350c07fa7930118aabb1f0cc
QIAN MA <maqian@hotmail.fr>
2020-05-24 16:24:26 +0200

    commit

--- src/main/java/com/example/community/controller/AutorizeController.java
+++ src/main/java/com/example/community/controller/AutorizeController.java
@@ -58,9 +58,9 @@ public class AutorizeController {
             return "redirect:/";//redirection
             // login success
         }
-        else {
+
             return "redirect:/";
             //login failed
-        }
+
     }
 }
